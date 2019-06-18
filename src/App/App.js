d404700f8dc1f4a48a89c00b097ddeb8f51ceec3
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-06-17 19:03:57 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+class App extends React.Component {
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-danger">Danger</button>
-      </header>
-    </div>
+<div className="App">
+<button className="btn btn-danger">GOATS</button>
+        <button className="btn btn-info">GOATS2</button>
+        <button className="btn btn-warning">GOATS3</button>
+        <button className="btn btn-primary">GOATS4</button>
+</div>
   );
+}
 }
 
 export default App;
